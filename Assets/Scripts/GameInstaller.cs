137ad4cf01eea6ff8ccc7c26c7e0f187bef0c4cf
Stepan Ustinov <Akynin99@gmail.com>
2025-05-16 15:27:36 +0700

    feat: create GameInstaller

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,17 @@
+﻿using DoodleLegend.Core;
+using UnityEngine;
+using Zenject;
+
+namespace DoodleLegend
+{
+    public class GameInstaller : MonoInstaller
+    {
+        [SerializeField] private PlayerController playerController;
+        public override void InstallBindings() 
+        {
+            Container.Bind<IInputHandler>().FromMethod(_ => InputHandlerFactory.Create()).AsSingle().NonLazy();
+        
+            Container.Bind<PlayerController>().FromInstance(playerController).AsSingle();
+        }
+    }
+}
